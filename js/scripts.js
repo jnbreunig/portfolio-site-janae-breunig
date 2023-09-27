ef1b31790fda7c823c43291bb96074e97f00a7ef
jnbreunig <144047412+jnbreunig@users.noreply.github.com>
2023-09-26 21:15:32 -0500

    Updated JS file per Williams helpful notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 console.log("Hi! Welcome to My Portfolio Site ");
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+	var isMobileBreakpoint = window.innerWidth <= 641
+
+  if (x.className === 'navtoggle' && isMobileBreakpoint) {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
   }
+}
